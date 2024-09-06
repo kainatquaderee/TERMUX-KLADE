4aff4bfec8a1c1ed30d6ccc4212089b531bfa864
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2024-09-06 17:29:17 +0600

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt update && apt upgrade 
+apt install sudo;
+
+groupadd storage;
+groupadd wheel;
+groupadd video;
+useradd -m -g users -G wheel,audio,video,storage -s /bin/bash user;
+echo "password for root";
+passwd;
+echo "password for user";
+passwd user;
+echo "user  ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+su user;
+cd;
+bash <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/install.sh);

1b9af4a903e7f1cbab02194db0817a8f327ae88b
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2024-09-06 16:18:14 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo KLADE FOR TERMUX
+......
+sleep 1
+...........
+sleep 1
+.....................
+sleep 1
+..................................
+pkg install $depends;
+proot-distro install debian
+proot-distro login --user root --shared-tmp debian;
+apt update && apt upgrade 
+apt install sudo;
+
+groupadd storage;
+groupadd wheel;
+groupadd video;
+useradd -m -g users -G wheel,audio,video,storage -s /bin/bash user;
+echo "password for root";
+passwd;
+echo "password for user";
+passwd user;
+echo "user  ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+su user;
+cd;
+bash <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/install.sh);

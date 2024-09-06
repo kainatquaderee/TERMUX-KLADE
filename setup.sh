9e206871d50430a1bcb5055c010c6470b45c6b67
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2024-09-06 17:28:18 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,18 +17,3 @@ cat <(curl -s https://raw.githubusercontent.com/kainatquaderee/TERMUX-KLADE/main
 chmod +x ~/.local/bin/startdesktop; 
 proot-distro install debian
 proot-distro login --user root --shared-tmp debian;
-apt update && apt upgrade 
-apt install sudo;
-
-groupadd storage;
-groupadd wheel;
-groupadd video;
-useradd -m -g users -G wheel,audio,video,storage -s /bin/bash user;
-echo "password for root";
-passwd;
-echo "password for user";
-passwd user;
-echo "user  ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-su user;
-cd;
-bash <(curl -s https://raw.githubusercontent.com/kainatquaderee/KLADE/main/install.sh);
